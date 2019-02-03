6d2a4c1a227897921f7196ef85c7292319fb5abb
Damian Karczewski <46339828+damian21kk@users.noreply.github.com>
2019-02-03 14:32:18 +0100

    Update Timer.m, added check_all to the script, made it look cleaner

--- Timer.m
+++ Timer.m
@@ -1,71 +1,122 @@
-clear
+clear;
 datetime.setDefaultFormats('default','yyyy-MM-dd hh:mm:ss');
+
 if exist('Timer.txt','file')
+  
     Told = readtable('Timer.txt');
+
 end
+
 while true
+
     prompt = 'What do you want to do? \nNew event[new], Check event[check], Exit the program[exit], Delete data[delete]:\n ';
     str = input(prompt,'s');
+
     if (str == "exit")
+
         if exit_function()==true
+  
             clear;
             return;
+  
         else
+  
             continue;
+  
         end
+
     elseif (isempty(str)) || (str ~= "exit") && (str ~= "check") && (str ~= "new") && (str ~= "delete")
+
         clc
         fprintf("Wrong command, please try again!\n");
         continue;
+
     elseif (str == "delete")
+
         delete_function();
+
     elseif (str == "new") && exist('Timer.txt','file') && exist('Told','var')
+
         T=[Told;New_event(Told)];
         writetable(T,'Timer.txt');
         writetable(T,'Timer.xlsx');
         T = readtable('Timer.txt');
         clc
         continue;
+
     elseif (str == "new") && exist('Timer.txt','file') && (exist('Told','var')==0)
+
         T=[T;New_event(T)];
         writetable(T,'Timer.txt');
         writetable(T,'Timer.xlsx');
         T = readtable('Timer.txt');
         clc
-    continue;
+        continue;
+
     elseif (str == "new") && (exist('Timer.txt','file')==0)
+
         T=new_table();
         writetable(T,'Timer.txt');
         writetable(T,'Timer.xlsx');
         T = readtable('Timer.txt');
         clc
         continue;
+
     elseif (str == "check") && (exist('Timer.txt','file')==0)
+
         clc
         fprintf('No data saved\n');
         continue;
+
     elseif (str == "check") && exist('Timer.txt','file')
+
         while true
-            prompt = 'What do you want to do? show events[show], check specific events[specific]: ';
+
+            prompt = 'What do you want to do? show events[show], check all events [all], check specific events[specific]: ';
             str = input(prompt,'s');
-            if (isempty(str)) || (str ~= "show") && (str ~= "specific")
+
+            if (isempty(str)) || (str ~= "show") && (str ~= "specific") && (str ~= "all")
+
                 clc
                 fprintf('Wrong command, please try again!\n')
                 continue;
+
             elseif (str == "show") && (exist('T','var')==0)
+
                 disp(Told);
                 break;
+
             elseif (str == "specific") && (exist('T','var')==0)
+
                check_specific(Told);
                break;
+
+            elseif (str == "all") && (exist('T','var')==0)
+
+               check_all(Told);
+               break;
+
             elseif (str == "show") && (exist('T','var')~=0)
+
                 disp(T);
                 break;
+
             elseif (str == "specific") && (exist('T','var')~=0)
+
                check_specific(T);
                break;
+
+            elseif (str == "all") && (exist('T','var')~=0)
+
+               check_all(T);
+               break;
+
             end
+
         end
+
         continue;
+
     end
+
 end
