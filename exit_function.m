dd9f921f54a06229700fd49391a4bb0a26e7e89e
Damian Karczewski <46339828+damian21kk@users.noreply.github.com>
2019-02-03 14:27:21 +0100

    Update exit_function.m, made it look cleaner

--- exit_function.m
+++ exit_function.m
@@ -1,20 +1,31 @@
 function [choice] = exit_function()
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%exit_function terminates the programme
+%   The function checks if the user truly wants to exit the program and if he/she does than it sends the user "choice" back to the main script
+
         while true
+        
             prompt = 'Are you sure you want to exit? Yes[Y]/No[N]:\n ';
             str = input(prompt,'s');
+         
             if (isempty(str))||(str~='Y') && (str~='N')
+   
                 fprintf("Wrong command, please try again!\n");
                 continue;
+   
             elseif str=='Y'
+   
                 clc;
                 choice=true;
                 break;
+   
             elseif str=='N'
+   
                 choice=false;
                 break;
-            end
+   
+             end
+        
         end
+
 end
 
