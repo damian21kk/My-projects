e87a27225b9eac0dc5f2845aa5dd501b1a2807ab
Damian Karczewski <46339828+damian21kk@users.noreply.github.com>
2019-02-03 14:25:25 +0100

    Update check_specific.m, made it look cleaner

--- check_specific.m
+++ check_specific.m
@@ -1,26 +1,45 @@
 function [] = check_specific(Told)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+%check_specific checks the remaining plan to only one specific event 
+%   This function checks time to an event defined by the user saved previously in the Timer.txt file
+
 datetime.setDefaultFormats('default','yyyy-MM-dd hh:mm:ss');
+
+%asking the user for the event
+
         while true
+
             prompt = 'What event do you want to check?: ';
             str = input(prompt,'s');
+
             if isempty(str)
+
                 fprintf("Please give the name of the event!\n");
                 continue;
+
             end
+
             A=strcmp(str,Told.Event);
+
             if all(A==0)
+
                 fprintf('This event is not saved\n');
                 continue;  
+
             else
+
                 break;
+
             end
+
         end
+        
+%calculating time remaining to the event
+
         Told.Properties.RowNames = Told.Event;
         t1 = datetime('now');
         t2 = datetime(table2array(Told(str,'Date')));
         dt = between(t1,t2); 
         disp(dt);
+
 end
 
