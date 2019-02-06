ce21019808bca36f896c46abc6fa769f1099a554
Damian Karczewski <46339828+damian21kk@users.noreply.github.com>
2019-02-06 10:12:53 +0100

    Replaced “==“ with strcmp() in Timer.m

--- Timer.m
+++ Timer.m
@@ -12,13 +12,13 @@ while true
     prompt = 'What do you want to do? \nNew event[new], Check event[check], Save Data as [save], Delete data[delete], Exit the program[exit]:\n ';
     str = input(prompt,'s');
 
-    if (isempty(str)) || (str ~= "save") && (str ~= "exit") && (str ~= "check") && (str ~= "new") && (str ~= "delete")
+    if (isempty(str)) || (strcmp(str,’save’)==0) && (strcmp(str,’exit’)==0) && (strcmp(str,’check’)==0) && (strcmp(str,’new’)==0) && (strcmp(str,’delete’)==0)
 
         clc
         fprintf("Wrong command, please try again!\n");
         continue;
     
-    elseif (str == "exit")
+    elseif (strcmp(str,’exit’)==1)
 
         if exit_function()==true
   
@@ -31,64 +31,64 @@ while true
   
         end
 
-    elseif (str == "save") && (exist('Told','var')==0) && (exist('T','var')==0)
+    elseif (strcmp(str,’save’)==1) && (exist('Told','var')==0) && (exist('T','var')==0)
        
         clc;
         fprintf("No data to be saved!\n");
         
-    elseif (str == "save") && exist('Told','var')
+    elseif (strcmp(str,’save’)==1) && exist('Told','var')
        
         save_function(Told);
         clc;
         
-    elseif(str == "save") && (exist('Told','var')==0)
+    elseif (strcmp(str,’save’)==1) && (exist('Told','var')==0)
         
         save_function(T);
         clc;
         
-    elseif (str == "delete")
+    elseif (strcmp(str,’delete’)==1)
 
         delete_function();
 
-    elseif (str == "new") && exist('Timer.txt','file') && exist('Told','var')
+    elseif (strcmp(str,’new’)==1) && exist('Timer.txt','file') && exist('Told','var')
 
         T=[Told;New_event(Told)];
         writetable(T,'Timer.txt');
         writetable(T,'Timer.xlsx');
         T = readtable('Timer.txt');
-        clc
+        clc;
         continue;
 
-    elseif (str == "new") && exist('Timer.txt','file') && (exist('Told','var')==0)
+    elseif (strcmp(str,’new’)==1) && exist('Timer.txt','file') && (exist('Told','var')==0)
 
         T=[T;New_event(T)];
         writetable(T,'Timer.txt');
         writetable(T,'Timer.xlsx');
         T = readtable('Timer.txt');
-        clc
+        clc;
         continue;
 
-    elseif (str == "new") && (exist('Timer.txt','file')==0)
+    elseif (strcmp(str,’new’)==1) && (exist('Timer.txt','file')==0)
 
         T=new_table();
         writetable(T,'Timer.txt');
         writetable(T,'Timer.xlsx');
         T = readtable('Timer.txt');
-        clc
+        clc;
         continue;
 
-    elseif (str == "check") && (exist('Timer.txt','file')==0)
+    elseif (strcmp(str,’check’)==1) && (exist('Timer.txt','file')==0)
 
-        clc
+        clc;
         fprintf('No data saved\n');
         continue;
 
-    elseif (str == "check") && (exist('T','var')==0)
+    elseif (strcmp(str,’check’)==1) && (exist('T','var')==0)
 
         check_function(Told);
         continue;
 
-    elseif (str == "check") && (exist('T','var')~=0)
+    elseif (strcmp(str,’check’)==1) && (exist('T','var')~=0)
 
         check_function(T);
         continue;
